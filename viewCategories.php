72731220f8cfa415860e189c6fb8803d84a1b1b3
SandaliSenarathne <sandalisenarathna@gmail.com>
2021-09-09 23:01:31 +0530

    viewCategories, viewItems

---
+++ viewCategories.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Categories</title>
+    <?php include("importStyles.php"); ?>
+</head>
+<body>
+    <div class="container-fluid" id="categories">
+    <div class="row">
+        <div class="col-md-12">
+            <h2 class="fadeInDown text-center">PRODUCT CATEGORIES</h1>
+                <div class="row fadeInDown mt-5">        
+                    <div class="col-lg-3 col-md-3 col-sm-12 my-1">
+                        <div class="card card-margin">
+                            <div class="bg-image hover-overlay ripple" data-ripple-color="light">
+                                <img
+                                    src="images/bg.jpg"
+                                    class="img-fluid center-cropped"
+                                    style="height:20vh; object-fit: cover; object-position: center; width:100%;"
+                                />
+                            </div>
+                            <div class="card-body" style="height:150px;">
+                                <h5 class="card-title text-center" >Wind Sreen</h5>
+                                <a href="#" class="btn btn-outline-primary" style="position:absolute; bottom:15px; left:15px; right:15px;">View Items</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-12 my-1">
+                        <div class="card card-margin">
+                            <div class="bg-image hover-overlay ripple" data-ripple-color="light">
+                                <img
+                                    src="images/bg.jpg"
+                                    class="img-fluid center-cropped"
+                                    style="height:20vh; object-fit: cover; object-position: center; width:100%;"
+                                />
+                            </div>
+                            <div class="card-body" style="height:150px;">
+                                <h5 class="card-title text-center" >Wheels</h5>
+                                <a href="#" class="btn btn-outline-primary" style="position:absolute; bottom:15px; left:15px; right:15px;">View Items</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-12 my-1">
+                        <div class="card card-margin">
+                            <div class="bg-image hover-overlay ripple" data-ripple-color="light">
+                                <img
+                                    src="images/bg.jpg"
+                                    class="img-fluid center-cropped"
+                                    style="height:20vh; object-fit: cover; object-position: center; width:100%;"
+                                />
+                            </div>
+                            <div class="card-body" style="height:150px;">
+                                <h5 class="card-title text-center" >Tayers</h5>
+                                <a href="#" class="btn btn-outline-primary" style="position:absolute; bottom:15px; left:15px; right:15px;">View Items</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-12 my-1">
+                        <div class="card card-margin">
+                            <div class="bg-image hover-overlay ripple" data-ripple-color="light">
+                                <img
+                                    src="images/bg.jpg"
+                                    class="img-fluid center-cropped"
+                                    style="height:20vh; object-fit: cover; object-position: center; width:100%;"
+                                />
+                            </div>
+                            <div class="card-body" style="height:150px;">
+                                <h5 class="card-title text-center" >Seats</h5>
+                                <a href="#" class="btn btn-outline-primary" style="position:absolute; bottom:15px; left:15px; right:15px;">View Items</a>
+                            </div>
+                        </div>
+                    </div> 
+                    <div class="col-lg-3 col-md-3 col-sm-12 my-1">
+                        <div class="card card-margin">
+                            <div class="bg-image hover-overlay ripple" data-ripple-color="light">
+                                <img
+                                    src="images/bg.jpg"
+                                    class="img-fluid center-cropped"
+                                    style="height:20vh; object-fit: cover; object-position: center; width:100%;"
+                                />
+                            </div>
+                            <div class="card-body" style="height:150px;">
+                                <h5 class="card-title text-center" >Tayers</h5>
+                                <a href="#" class="btn btn-outline-primary" style="position:absolute; bottom:15px; left:15px; right:15px;">View Items</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-12 my-1">
+                        <div class="card card-margin">
+                            <div class="bg-image hover-overlay ripple" data-ripple-color="light">
+                                <img
+                                    src="images/bg.jpg"
+                                    class="img-fluid center-cropped"
+                                    style="height:20vh; object-fit: cover; object-position: center; width:100%;"
+                                />
+                            </div>
+                            <div class="card-body" style="height:150px;">
+                                <h5 class="card-title text-center" >Seats</h5>
+                                <a href="#" class="btn btn-outline-primary" style="position:absolute; bottom:15px; left:15px; right:15px;">View Items</a>
+                            </div>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <?php include("importScripts.php"); ?>
+
+</body>
+</html>
---
+++ viewItems.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Categories</title>
+    <?php include("importStyles.php"); ?>
+</head>
+<body>
+    <div class="container-fluid py-5 px-5" id="items">
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-12">
+            <h2 class="fadeInDown text-center">Items</h1>
+            <table class="table">
+                <tr>
+                    <td><img src="images/bg.jpg" width="75px"></td>
+                    <td>Wheel set</td>
+                    <td>Lorem ipsum dolor sit quos pariatur excepturi dignissimos veritatis </td>
+                    <td>7500 LKR</td>
+                    <td><button type="button" class="btn btn-outline-success" onClick="#">Add to cart</button></td>
+                </tr>
+                <tr>
+                    <td><img src="images/bg.jpg" width="75px"></td>
+                    <td>Wheel set</td>
+                    <td>Lorem ipsum dolor sit quos pariatur excepturi dignissimos veritatis </td>
+                    <td>7500 LKR</td>
+                    <td><button type="button" class="btn btn-outline-success" onClick="#">Add to cart</button></td>
+                </tr>
+                <tr>
+                    <td><img src="images/bg.jpg" width="75px"></td>
+                    <td>Wheel set</td>
+                    <td>Lorem ipsum dolor sit quos pariatur excepturi dignissimos veritatis </td>
+                    <td>7500 LKR</td>
+                    <td><button type="button" class="btn btn-outline-success" onClick="#">Add to cart</button></td>
+                </tr>
+                <tr>
+                    <td><img src="images/bg.jpg" width="75px"></td>
+                    <td>Wheel set</td>
+                    <td>Lorem ipsum dolor sit quos pariatur excepturi dignissimos veritatis </td>
+                    <td>7500 LKR</td>
+                    <td><button type="button" class="btn btn-outline-success" onClick="#">Add to cart</button></td>
+                </tr>
+                <tr>
+                    <td><img src="images/bg.jpg" width="75px"></td>
+                    <td>Wheel set</td>
+                    <td>Lorem ipsum dolor sit quos pariatur excepturi dignissimos veritatis </td>
+                    <td>7500 LKR</td>
+                    <td><button type="button" class="btn btn-outline-success" onClick="#">Add to cart</button></td>
+                </tr>
+            </table>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+            <h2 class="fadeInDown text-center">My Cart</h1>
+            <table class="table">
+                <tr>
+                    <td><img src="images/bg.jpg" width="75px"></td>
+                    <td>Wheel set</td>
+                    <td>Lorem ipsum dolor sit quos pariatur excepturi dignissimos veritatis </td>
+                    <td>7500 LKR</td>
+                    <td><button type="button" class="btn btn-outline-danger" onClick="#">Remove from cart</button></td>
+                </tr>
+                <tr>
+                    <td><img src="images/bg.jpg" width="75px"></td>
+                    <td>Wheel set</td>
+                    <td>Lorem ipsum dolor sit quos pariatur excepturi dignissimos veritatis </td>
+                    <td>7500 LKR</td>
+                    <td><button type="button" class="btn btn-outline-danger" onClick="#">Remove from cart</button></td>
+                </tr>
+                <tr>
+                    <td><img src="images/bg.jpg" width="75px"></td>
+                    <td>Wheel set</td>
+                    <td>Lorem ipsum dolor sit quos pariatur excepturi dignissimos veritatis </td>
+                    <td>7500 LKR</td>
+                    <td><button type="button" class="btn btn-outline-danger" onClick="#">Remove from cart</button></td>
+                </tr>
+            </table>
+        </div>
+    </div>
+    </div>
+
+    <?php include("importScripts.php"); ?>
+
+</body>
+</html>
