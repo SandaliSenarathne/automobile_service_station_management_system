a2ca73d9601e506c8c5df5275af26b0a49623626
SandaliSenarathne <sandalisenarathna@gmail.com>
2021-09-12 22:05:37 +0530

    error sessions in addCategoryHandler and addItemHandler, view categories minor ui changes, items section in viewItems.php done

--- viewItems.php
+++ viewItems.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    require_once("db.php");
+    $category = $_GET['category'];
+    $sql = "SELECT * FROM item WHERE category_id=$category AND stock>0;";
+    $result = $conn->query($sql);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,41 +17,27 @@
         <div class="col-lg-6 col-md-6 col-sm-12">
             <h2 class="fadeInDown text-center">Items</h1>
             <table class="table">
+                <?php
+                if($result->num_rows > 0){
+                    while($row = $result->fetch_assoc()){
+                ?>
                 <tr>
-                    <td><img src="images/bg.jpg" width="75px"></td>
-                    <td>Wheel set</td>
-                    <td>Lorem ipsum dolor sit quos pariatur excepturi dignissimos veritatis </td>
-                    <td>7500 LKR</td>
+                    <td><img src="<?php echo($row['thumbnail']) ?>" width="75px"></td>
+                    <td><?php echo($row['name']) ?></td>
+                    <td><?php echo($row['description']) ?></td>
+                    <td><?php echo($row['price']) ?></td>
                     <td><button type="button" class="btn btn-outline-success" onClick="#">Add to cart</button></td>
                 </tr>
-                <tr>
-                    <td><img src="images/bg.jpg" width="75px"></td>
-                    <td>Wheel set</td>
-                    <td>Lorem ipsum dolor sit quos pariatur excepturi dignissimos veritatis </td>
-                    <td>7500 LKR</td>
-                    <td><button type="button" class="btn btn-outline-success" onClick="#">Add to cart</button></td>
-                </tr>
-                <tr>
-                    <td><img src="images/bg.jpg" width="75px"></td>
-                    <td>Wheel set</td>
-                    <td>Lorem ipsum dolor sit quos pariatur excepturi dignissimos veritatis </td>
-                    <td>7500 LKR</td>
-                    <td><button type="button" class="btn btn-outline-success" onClick="#">Add to cart</button></td>
-                </tr>
-                <tr>
-                    <td><img src="images/bg.jpg" width="75px"></td>
-                    <td>Wheel set</td>
-                    <td>Lorem ipsum dolor sit quos pariatur excepturi dignissimos veritatis </td>
-                    <td>7500 LKR</td>
-                    <td><button type="button" class="btn btn-outline-success" onClick="#">Add to cart</button></td>
-                </tr>
-                <tr>
-                    <td><img src="images/bg.jpg" width="75px"></td>
-                    <td>Wheel set</td>
-                    <td>Lorem ipsum dolor sit quos pariatur excepturi dignissimos veritatis </td>
-                    <td>7500 LKR</td>
-                    <td><button type="button" class="btn btn-outline-success" onClick="#">Add to cart</button></td>
-                </tr>
+                <?php
+                    }
+                }else{
+                ?>
+                    <tr>
+                        <td><h2 class="text-center">No Items found in this category.</h2></td>
+                    </tr>
+                <?php
+                }
+                ?>
             </table>
         </div>
         <div class="col-lg-6 col-md-6 col-sm-12">
