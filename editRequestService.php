af68830ab35af3c58778886cbabb1a7460192703
Sachitha Hirushan <shpsachitha@gmail.com>
2021-10-17 22:14:38 +0530

    take data from backend

--- editRequestService.php
+++ editRequestService.php
@@ -8,16 +8,20 @@
         
     }else{
             $id = $_GET['id'];
-            if($id == 1){
+            $sql = "SELECT * FROM booking WHERE id = $id";
+            // get type
+            require_once("db.php");
+            $result = $conn->query($sql);
+            $row = $result->fetch_assoc();
+            if($row['service_type'] == 1){
                 $type = "Normal Service";
-            }else if($id == 2){
+            }else if($row['service_type'] == 2){
                 $type = "Repair Service";
-            }else if($id == 3){
+            }else if($row['service_type'] == 3){
                 $type = "Breakdown Service";
             }else{
                 $type = "Modification Service";
             }
-
         ?>
 
         
@@ -39,26 +43,26 @@
             <div class="card-body">
                 <form class="login-form">
                     <div class="form-group">
-                        <input type="text" class="form-control" id="vehicleNumber" placeholder="Vehicle Number" required>
+                        <input type="text" class="form-control" id="vehicleNumber" placeholder="Vehicle Number" value="<?php echo $row['vehicle_number']?>" required>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" id="vehicleBrand" placeholder="Vehicle Brand" required>
+                        <input type="text" class="form-control" id="vehicleBrand" placeholder="Vehicle Brand" value="<?php echo $row['vehicle_brand']?>" required>
                     </div>
                     <div class="form-group">
-                        <input type="text" class="form-control" id="vehicleModel" placeholder="Vehicle Model" required>
+                        <input type="text" class="form-control" id="vehicleModel" placeholder="Vehicle Model" value="<?php echo $row['vehicle_model']?>" required>
                     </div>
                     <div class="form-group">
                         <p style="text-align:left">Select Date</p>
-                        <input type="date" class="form-control" id="rdate" placeholder="Vehicle Model" required>
+                        <input type="date" class="form-control" id="rdate" placeholder="Vehicle Model"  value="<?php echo $row['date']?>"  required>
                     </div>
                     <div class="form-group">
                         
                         <p style="text-align:left">Select Time</p>
-                        <input type="time" class="form-control" id="rtime" placeholder="Vehicle Model" required>
+                        <input type="time" class="form-control" id="rtime" placeholder="Vehicle Model"  value="<?php echo $row['time']?>"  required>
                     </div>
                     <div class="form-group">
                         
-                        <textarea class="form-control" id="message" placeholder="Message (optional)" max="1000" rows="2"></textarea>
+                        <textarea class="form-control" id="message" placeholder="Message (optional)"   max="1000" rows="2"> <?php echo $row['message']?></textarea>
                     </div>
                     <p id="err" style="color:red"></p>
                     <p id="success" style="color:green"></p>
